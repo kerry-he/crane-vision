eef40dcfdc14e6f27ff885d5f3dcd2d4024cd21d
Kerry He <kerry-he@github.com>
2021-01-22 19:05:52 +1100

    Made robot goto initial condition and go to waypoints relative to that point

--- yumi/yumi_test.py
+++ yumi/yumi_test.py
@@ -1,22 +1,44 @@
 import numpy as np
+from pyquaternion import Quaternion
 
 from yumipy import YuMiRobot
 from yumipy import YuMiState
+from yumipy import YuMiMotionPlanner
 
 from autolab_core import RigidTransform
+
+def generate_waypoints():
+	# Generate a pendulum-like motion for the robot to folow
+	origin = np.array([0., 0., 0.20])
+	l = 0.20
+
+	waypoints = []
+
+	for i in range(50):
+		theta = np.cos(i / 10.)
+
+		x = origin[0] - l * np.sin(theta)
+		y = origin[1]
+		z = origin[2] - l * np.cos(theta)
+
+		R = Quaternion(axis=[0., 1., 0.], radians=theta).rotation_matrix 
+		t = np.array([x, y, z])
+
+		T = RigidTransform(rotation=R, translation=t, from_frame="world")
+		waypoints.append(T)
+
+	return waypoints
+
 
 if __name__ == '__main__':
 	# YuMi Instantiation
 	print("Instantiate YuMi")
-	Y = YuMiRobot()
-
-
-	print(Y.left.get_pose())
-	print(Y.left.get_state())
+	Y = YuMiRobot()#(arm_type='remote')
+	# planner = YuMiMotionPlanner()
 
 	# Set speed of both arms
-	Y.set_v(50)
-	
+	Y.set_v(200)
+	# Y.left.set_motion_planner()
 
 	# PoseCalibration
 	PoseCalibrationLeft = YuMiState([0, -130, 30, 0, 40, 0, 135])
@@ -25,42 +47,26 @@ if __name__ == '__main__':
 	# Set robot to PoseCalibration
 	raw_input("Press enter to move to begin pose...")
 
-	left_pose = Y.left.get_pose()
+	Y.left.goto_state(PoseCalibrationLeft)
 
-	T = RigidTransform(rotation=np.eye(3), translation=np.array([0.1, 0., 0.]), from_frame="world")
-	target_pose = T * left_pose
+	# Set robot to origin
+	raw_input("Press enter to move to begin pose...")
 
-	print(target_pose)
-
-	Y.left.goto_pose(target_pose)
-
-
-
-	left_pose = Y.left.get_pose()
-
-	T = RigidTransform(rotation=np.eye(3), translation=np.array([-0.2, 0., 0.]), from_frame="world")
-	target_pose = T * left_pose
-
-	print(target_pose)
-
-	Y.left.goto_pose(target_pose)
+	R = RigidTransform.rotation_from_quaternion([0., 0.707, 0.707, 0.])
+	R_origin = RigidTransform(rotation=R, from_frame="world")
+	t = np.array([0.50, 0.05, 0.20])
+	t_origin = RigidTransform(translation=t, from_frame="world")
 	
-	# # Y.right.goto_state(PoseCalibrationRight)
-	
-	# left_pose = Y.left.get_pose()
+	T_origin = t_origin * R_origin 
 
-	# print(left_pose.translation)
-	# print(left_pose.rotation)
+	print(T_origin)
+	Y.left.goto_pose(T_origin)
 
-	# T = RigidTransform(rotation=left_pose.rotation, translation=left_pose.translation + np.array([0.2, 0.1, 0.1]))
+	waypoints = generate_waypoints()
 
-	# Y.left.goto_pose(T)
-
-
-	# Y.left.goto_pose(T)
-
-	# left_pose = Y.left.get_pose()
-
-	# print(left_pose.translation)
-	# print(left_pose.rotation)
+	for w_T in waypoints:
+		print(w_T)
+		print(t_origin * w_T * R_origin)
+		raw_input()
+		Y.left.goto_pose(t_origin * w_T * R_origin)
 
